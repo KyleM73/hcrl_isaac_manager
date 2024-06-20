974bbabbc17c683361bc3eb8032a8ee340b8b0bf
KyleM73 <kyle.morgenstein@gmail.com>
2024-06-19 23:58:53 -0500

    mark safe before training

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-/workspace/isaaclab/isaaclab.sh -p /workspace/isaaclab/source/extensions/isaaclab.hcrl/scripts/rsl_rl/train.py $@
+# mark extensions as safe
+git config --global --add safe.directory /workspace/isaaclab/source/extensions/isaaclab.hcrl
+# train
+/workspace/isaaclab/isaaclab.sh -p /workspace/isaaclab/source/extensions/isaaclab.hcrl/scripts/rsl_rl/train.py --headless --enable_cameras $@
