e71cc804f6785dfd919b48b0f1bf19f09f110023
KyleM73 <kyle.morgenstein@gmail.com>
2024-06-20 19:47:46 -0500

    fix path to train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,4 +3,4 @@
 # mark extensions as safe
 git config --global --add safe.directory /workspace/isaaclab/source/extensions/isaaclab.hcrl
 # train
-/workspace/isaaclab/isaaclab.sh -p /workspace/isaaclab/source/extensions/isaaclab.hcrl/scripts/rsl_rl/train.py --headless --enable_cameras $@
+/workspace/isaaclab/isaaclab.sh -p /workspace/isaaclab/source/extensions/isaaclab.hcrl/scripts/train.py --headless --enable_cameras $@
