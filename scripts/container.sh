a97d3872be4ce60153cb14892295a384f267de66
KyleM73 <kyle.morgenstein@gmail.com>
2024-09-16 20:30:12 -0500

    streamline wandb env setting

--- scripts/container.sh
+++ scripts/container.sh
@@ -3,16 +3,11 @@
 # make the cwd the pwd
 cd "${0%/*}"
 # export wandb api keys
-if ! grep -Fxq "# export wandb api keys" ../resources/IsaacLab/docker/Dockerfile.base
-then
-    echo "" >> ../resources/IsaacLab/docker/Dockerfile.base
-    echo "# export wandb api keys" >> ../resources/IsaacLab/docker/Dockerfile.base
-    cat ./wandb >> ../resources/IsaacLab/docker/Dockerfile.base
-fi
+wandb_env_file="$(pwd)/.env.wandb"
 # take git ownership of the hcrl extension
-sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/extensions/isaaclab.hcrl/
+# sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/extensions/isaaclab.hcrl/
 # turn off x11 mode
 ssh_mode='"0"'
-sed -i "s/^__ISAACLAB_X11_FORWARDING_ENABLED.*/__ISAACLAB_X11_FORWARDING_ENABLED: ${ssh_mode}/" ../resources/IsaacLab/docker/.container.yaml
+sed -i "s/^x11_forwarding_enabled.*/x11_forwarding_enabled: ${ssh_mode}/" ../resources/IsaacLab/docker/.container.cfg
 # start the container
-python3 "../resources/IsaacLab/docker/container.py" "${@:1}"
+python3 ../resources/IsaacLab/docker/container.py "${@:1}" --env-file $wandb_env_file
