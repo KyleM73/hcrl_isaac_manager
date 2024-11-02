b2be9e88e43a53389f6cf4f4209030dbbe497a58
KyleM73 <kyle.morgenstein@gmail.com>
2024-11-01 20:15:05 -0500

    typo

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -3,10 +3,10 @@
 # make the cwd the pwd
 cd "$(dirname "$0")"
 
-cp cluster/.env.cluster ../resources/Isaaclab/docker/cluster/.env.cluster
-cp .env.wandb  ../resources/Isaaclab/docker/cluster/.env.wandb
-cp cluster/cluster_interface.sh  ../resources/Isaaclab/docker/cluster/cluster_interface.sh
-cp cluster/run_singularity.sh  ../resources/Isaaclab/docker/cluster/run_singularity.sh
-cp cluster/submit_job_slurm.sh  ../resources/Isaaclab/docker/cluster/submit_job_slurm.sh
+cp cluster/.env.cluster ../resources/IsaacLab/docker/cluster/.env.cluster
+cp .env.wandb  ../resources/IsaacLab/docker/cluster/.env.wandb
+cp cluster/cluster_interface.sh  ../resources/IsaacLab/docker/cluster/cluster_interface.sh
+cp cluster/run_singularity.sh  ../resources/IsaacLab/docker/cluster/run_singularity.sh
+cp cluster/submit_job_slurm.sh  ../resources/IsaacLab/docker/cluster/submit_job_slurm.sh
 
-../resources/Isaaclab/docker/cluster/cluster_interface.sh "${@:1}"
+../resources/IsaacLab/docker/cluster/cluster_interface.sh "${@:1}"
