405ead738dcb3dcf0ea75757a8aa86f3c9948fe7
KyleM73 <kyle.morgenstein@gmail.com>
2024-12-06 21:54:58 -0600

    fixed container not picking up wandb automatically

--- scripts/container.sh
+++ scripts/container.sh
@@ -4,10 +4,11 @@
 cd "$(dirname "$0")"
 # export wandb api keys
 wandb_env_file="$(pwd)/.env.wandb"
+cat $wandb_env_file >> ../resources/IsaacLab/docker/.env.base
 # take git ownership of the hcrl extension
-# sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/extensions/isaaclab.hcrl/
+sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/extensions/isaaclab.hcrl/
 # turn off x11 mode
 ssh_mode='"0"'
 sed -i "s/^x11_forwarding_enabled.*/x11_forwarding_enabled: ${ssh_mode}/" ../resources/IsaacLab/docker/.container.cfg
 # start the container
-python3 ../resources/IsaacLab/docker/container.py "${@:1}" --env-file $wandb_env_file
+python3 ../resources/IsaacLab/docker/container.py "${@:1}" base
