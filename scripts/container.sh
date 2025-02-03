f59f60aa01520ff1a7adfaaef0dbd7bc8ec304f9
KyleM73 <kyle.morgenstein@gmail.com>
2025-02-02 18:06:45 -0600

    bump for IsaacLab 2.0

--- scripts/container.sh
+++ scripts/container.sh
@@ -3,12 +3,12 @@
 # make the cwd the pwd
 cd "$(dirname "$0")"
 # mark hcrl git directories as safe
-git config --global --add safe.directory ../resources/IsaacLab/source/extensions/isaaclab.hcrl
+git config --global --add safe.directory ../resources/IsaacLab/source/hcrl_isaaclab/
 # export wandb api keys
 wandb_env_file="$(pwd)/.env.wandb"
 cat $wandb_env_file >> ../resources/IsaacLab/docker/.env.base
 # take git ownership of the hcrl extension
-sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/extensions/isaaclab.hcrl/
+sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/hcrl_isaaclab/
 # turn off x11 mode
 ssh_mode='"0"'
 sed -i "s/^x11_forwarding_enabled.*/x11_forwarding_enabled: ${ssh_mode}/" ../resources/IsaacLab/docker/.container.cfg
