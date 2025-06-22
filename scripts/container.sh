fdf359d1160e3e40e326e724b49e30b1d58219fc
KyleM73 <kyle.morgenstein@gmail.com>
2025-06-22 12:30:28 -0500

    smarter wandb env var passing

--- scripts/container.sh
+++ scripts/container.sh
@@ -6,7 +6,10 @@ cd "$(dirname "$0")"
 git config --global --add safe.directory ../resources/IsaacLab/source/hcrl_isaaclab/
 # export wandb api keys
 wandb_env_file="$(pwd)/.env.wandb"
-cat $wandb_env_file >> ../resources/IsaacLab/docker/.env.base
+target_env_file="../resources/IsaacLab/docker/.env.base"
+if ! grep -Fxq -f "$wandb_env_file" "$target_env_file"; then
+    cat "$wandb_env_file" >> "$target_env_file"
+fi
 # take git ownership of the hcrl extension
 sudo chown -R "${USER:-$(id -un)}" ../resources/IsaacLab/source/hcrl_isaaclab/
 # turn off x11 mode
